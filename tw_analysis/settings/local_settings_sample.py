d7ac0477b5279a086ccf2fd772cdb6ecb9a39aa2
Imanoel Ali <mehot1@gmail.com>
2017-07-03 07:53:00 +0430

    update local_settings parametr

--- tw_analysis/settings/local_settings_sample.py
+++ tw_analysis/settings/local_settings_sample.py
@@ -1,26 +1,30 @@
 import os
-
 import tweepy
 
+# DJANGO SETTING SECTION
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Twitter SECTION
+
+# communication api
 consumer_key = ""
 consumer_secret = ""
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
 access_token = ""
 access_token_secret = ""
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
 # Fetch data api
 consumer_key_data = ""
 consumer_secret_data = ""
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
 access_token_data = ""
 access_token_secret_data = ""
 
@@ -29,6 +33,10 @@ auth.set_access_token(access_token_data, access_token_secret_data)
 
 api_data = tweepy.API(auth)
 
+# admin twitter account for sending direct message
+ADMIN_TW_ACCOUNT = ''
+
+# DATABASE SECTION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -39,14 +47,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # MongoDB
 mongodb_authentication_source = ''
