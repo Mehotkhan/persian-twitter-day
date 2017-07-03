7375cbb9a04b9fe3f07916e54bc9620551be7d4a
Imanoel Ali <mehot1@gmail.com>
2017-07-03 07:53:44 +0430

    update text cloud generator + add data and time input

--- tw/tasks.py
+++ tw/tasks.py
@@ -1,14 +1,14 @@
 import datetime
-from celery import shared_task
 import tweepy
-
+from celery import shared_task
 from tw.views import TweetCloud
 from tw_analysis.settings.local_settings import api
+from tw_analysis.settings.local_settings import ADMIN_TW_ACCOUNT
 
 
 @shared_task
 def auto_follow_back():
-    api.send_direct_message(user='last_elvish', text='i\'m going to follow back :)')
+    api.send_direct_message(user=ADMIN_TW_ACCOUNT, text='i\'m going to follow back :)')
     for follower in tweepy.Cursor(api.followers).items():
         follower.follow()
 
@@ -17,11 +17,12 @@ def auto_follow_back():
 def keep_alive():
     #
     now_time = datetime.datetime.now().strftime('%d-%b-%Y | %H:%M:%S')
-    api.send_direct_message(user='last_elvish', text='i\'m here \n {}'.format(now_time))
+    api.send_direct_message(user=ADMIN_TW_ACCOUNT, text='i\'m here \n {}'.format(now_time))
 
 
 @shared_task
-def tweet_cloud():
+def tweet_cloud(from_date=None, to_date="Today", from_time=None, to_time="Now", max_words=1000):
     command_cloud = TweetCloud()
-    command_cloud.generate()
+    command_cloud.generate(from_date=from_date, to_date=to_date, from_time=from_time, to_time=to_time,
+                           max_words=max_words)
     command_cloud.send()
--- tw/views.py
+++ tw/views.py
@@ -2,7 +2,7 @@ import datetime
 import jdatetime
 from mongoengine import Q
 from tw.mongo_model import Analysis
-from tw_analysis.settings.local_settings import api
+from tw_analysis.settings.local_settings import api, ADMIN_TW_ACCOUNT
 from os import path
 from wordcloud import WordCloud
 import arabic_reshaper
@@ -15,22 +15,33 @@ class TweetCloud(object):
         self.file_names = []
         self.d = path.dirname(__file__)
         self.all_tweets_count = None
+        self.font_path = path.join(self.d, 'fonts', 'Vazir-Light.ttf')
+        self.from_date = None
+        self.to_date = None
 
     @staticmethod
     def is_perisan(s):
         return u'\u0600' <= s <= u'\u06FF'
 
-    def generate(self):
-        api.send_direct_message(user='last_elvish', text='hey , i\'m going to generate text CLOUD :*')
-        font_path = path.join(self.d, 'fonts', 'Vazir-Light.ttf')
-        # Read the whole text.
-        yesterday = datetime.date.today() - datetime.timedelta(1)
-        tomorow = datetime.date.today() + datetime.timedelta(1)
-        today = datetime.date.today()
+    def generate(self, from_date=None, to_date="Today", from_time=None, to_time="Now", max_words=1000):
+        api.send_direct_message(user=ADMIN_TW_ACCOUNT, text='hey , i\'m going to generate text CLOUD :*')
+        if from_date and to_date:
+            if from_date == to_date and from_date == "Today":
+                # Read the whole text.
+                self.from_date = datetime.date.today() - datetime.timedelta(1)
+                self.to_date = datetime.date.today()
+            elif isinstance(float, from_date) and to_date == "Today":
+                self.from_date = datetime.date.today() + datetime.timedelta(from_date)
+                self.to_date = datetime.date.today()
+        if from_time and to_time:
+            if isinstance(float, from_time) and to_time == "Now":
+                self.from_date = datetime.datetime.now() + datetime.timedelta(hours=from_time)
+                self.to_date = datetime.datetime.now()
+
         all_tweets = Analysis.objects(
-            Q(create_date__lt=today)
+            Q(create_date__lt=self.to_date)
             &
-            Q(create_date__gte=yesterday)
+            Q(create_date__gte=self.from_date)
 
         ).all()
         self.all_tweets_count = len(all_tweets)
@@ -53,8 +64,8 @@ class TweetCloud(object):
                          open((path.join(self.d, 'stop_words.txt')), encoding='utf-8').read().split('\n')])
 
         self.tweet_cloud = WordCloud(
-            font_path=font_path,
-            max_words=1000,
+            font_path=self.font_path,
+            max_words=max_words,
             stopwords=stopwords,
             # mask=mask,
             margin=0,
@@ -67,7 +78,7 @@ class TweetCloud(object):
         ).generate(text)
 
     def send(self):
-        api.send_direct_message(user='last_elvish', text='hey , i\'m going to send text CLOUD :*')
+        api.send_direct_message(user=ADMIN_TW_ACCOUNT, text='hey , i\'m going to send text CLOUD :*')
         # The pil way (if you don't have matplotlib)
         filename = datetime.datetime.today().strftime('%Y-%m-%d')
         image = (path.join(self.d, filename + '.png'))
@@ -84,4 +95,4 @@ class TweetCloud(object):
                                                                 date.strftime(
                                                                     "%d-%b-%Y "))
         api.update_status(status=status_text, media_ids=media_ids)
-        api.send_direct_message(user='last_elvish', text='hora , text cloud image sends :**')
+        api.send_direct_message(user=ADMIN_TW_ACCOUNT, text='hora , text cloud image sends :**')
