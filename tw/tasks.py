37a964846efc1ba0b09384db2ccd853aaea216d1
Imanoel Ali <mehot1@gmail.com>
2017-07-12 01:36:55 +0430

    fix wordcloud task

--- tw/tasks.py
+++ tw/tasks.py
@@ -21,8 +21,18 @@ def keep_alive():
 
 
 @shared_task
-def tweet_cloud(from_date=None, to_date="Today", from_time=None, to_time="Now", max_words=1000):
+def tweet_cloud(from_date=None, from_time=None, max_words=1000):
+    f_date = None
+    f_time = None
+    if from_date == 0:
+        f_date = None
+    else:
+        f_date = from_date
+    if from_time == 0:
+        f_time = None
+    else:
+        from_time = from_time
     command_cloud = TweetCloud()
-    command_cloud.generate(from_date=from_date, to_date=to_date, from_time=from_time, to_time=to_time,
+    command_cloud.generate(from_date=None, from_time=-24,
                            max_words=max_words)
     command_cloud.send()
