3ae2dd5cbe2e425dfd1389cd35e3118826311ac1
Imanoel Ali <mehot1@gmail.com>
2017-07-17 01:52:21 +0430

    add hazm data cleaner

--- tw/text_cleaner.py
+++ tw/text_cleaner.py
@@ -5,14 +5,24 @@ from os import path
 
 class FetchText(object):
     @staticmethod
-    def generate(text):
+    def clean_text(text):
+        all_words = []
+        text = text.split()
+        for words in text:
+            if u'\u0600' <= words <= u'\u06FF':
+                all_words.append(words)
+        return ' '.join(all_words)
+
+    @classmethod
+    def generate(cls, text):
         # first we split sentence of tweet
         sentences = sent_tokenize(text)
         # now find every section of sentence
         text_data = []
         for sentence in sentences:
             normalizer = Normalizer()
-            text_normal = normalizer.normalize(sentence)
+            clean_sentence = cls.clean_text(text=sentence)
+            text_normal = normalizer.normalize(clean_sentence)
             tagger = POSTagger(model=path.dirname(__file__) + '/resources/postagger.model')
             taged = tagger.tag(word_tokenize(text_normal))
             text_data.append(taged)
