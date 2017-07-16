e190b02ec5ea9716ae96a28c9acbd10b82f89ac8
Imanoel Ali <mehot1@gmail.com>
2017-07-17 02:17:59 +0430

    update follow user

--- tw/management/commands/follow_user.py
+++ tw/management/commands/follow_user.py
@@ -13,36 +13,31 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         friends = api.friends_ids(api.me().id)
+        friends_user = api.friends_ids(screen_name=options['user_id'])
+        final_ids = [user for user in friends_user if user not in friends and user != api.me().id]
         print("You follow", len(friends), "users")
-        while True:
+        print("friends_user", len(friends_user), "users")
+        print("number of news follow = ", len(final_ids), "users")
+        # exit()
+        # while True:
+        #     try:
+        # user_follower = tweepy.Cursor(api.friends, screen_name=options['user_id']).items()
+        # user_ids = []
+        # print(len(user_follower))
+        # print('start following {} user'.format(len(list(user_follower))))
+        for follower in final_ids:
             try:
-                user_follower = tweepy.Cursor(api.friends, screen_name=options['user_id']).items()
-                # print('start following {} user'.format(len(list(user_follower))))
-                for follower in user_follower:
-                    if follower.id != api.me().id:
-                        if follower.id in friends:
-                            print("You already follow", follower.screen_name)
-                        elif follower.id not in friends:
-                            try:
-                                follower.follow()
-                                print("Started following", follower.screen_name)
+                api.create_friendship(follower)
+                print("Started following", follower)
 
-                            except tweepy.RateLimitError:
-                                for i in range(2 * 60, 0, -1):
-                                    time.sleep(1)
-                                    sys.stdout.write("\r")
-                                    sys.stdout.write("{:2d} seconds remaining.".format(i))
-                                    sys.stdout.flush()
-                                continue
-                            except tweepy.TweepError:
-                                continue
-                        else:
-                            print('some error')
             except tweepy.RateLimitError:
                 for i in range(2 * 60, 0, -1):
                     time.sleep(1)
                     sys.stdout.write("\r")
                     sys.stdout.write("{:2d} seconds remaining.".format(i))
                     sys.stdout.flush()
-                # time.sleep(60 * 2)
                 continue
+            except tweepy.TweepError:
+                continue
+        else:
+            print('some error')
