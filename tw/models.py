65bea1fa287c1a664c939bc13ec17cf6f1398d4c
Imanoel Ali <mehot1@gmail.com>
2017-07-18 19:25:35 +0430

    fix bug

--- tw/models.py
+++ tw/models.py
@@ -84,10 +84,10 @@ class StdOutListener(StreamListener):
             tweet.source = re.findall(r'<a .*>(.*)</a>', data_json['source'])[0]
             tweet.is_quote_status = data_json['is_quote_status']
             tweet.media_type = data_json['entities']['media'] if data_json['entities'].get('media') else ''
-            tweet.retweet_count = data_json['retweeted_status']['retweet_count'] if data_json['retweeted_status'].get(
-                'retweet_count') else 0
-            tweet.favorite_count = data_json['retweeted_status']['favorite_count'] if data_json['retweeted_status'].get(
-                'favorite_count') else 0
+            tweet.retweet_count = data_json['retweeted_status']['retweet_count'] if data_json.get(
+                'retweeted_status') else 0
+            tweet.favorite_count = data_json['retweeted_status']['favorite_count'] if data_json.get(
+                'retweeted_status') else 0
             tweet.user_mentions = data_json['entities']['user_mentions'] if data_json['entities'].get(
                 'user_mentions') else []
             tweet.hashtags = data_json['entities']['hashtags'] if data_json['entities'].get(
