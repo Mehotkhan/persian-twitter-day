e0df02daac2db6e27b01d34ea7e5ca221815d50d
Imanoel Ali <mehot1@gmail.com>
2017-07-19 21:58:51 +0430

    auto redirect to admin

--- tw_analysis/urls.py
+++ tw_analysis/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/admin')),
     url(r'^admin/', admin.site.urls),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
