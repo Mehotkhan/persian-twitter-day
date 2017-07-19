505c9cfe5b86dc884f755432b58bdf288b87d1e1
Imanoel Ali <mehot1@gmail.com>
2017-07-19 21:57:27 +0430

    add jet.dashboard

--- tw_analysis/urls.py
+++ tw_analysis/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
 ]
