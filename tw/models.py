1b0e84344f33534e715432cab7fecbc4d2905059
Imanoel Ali <mehot1@gmail.com>
2017-07-21 00:06:39 +0430

    Fix BUG ?

--- tw/models.py
+++ tw/models.py
@@ -67,40 +67,39 @@ class StdOutListener(StreamListener):
         data_json = json.loads(data)
         if data_json.get('text'):
             # check dumplicate tweet
-            if Analysis.objects(tweet_id=data_json['id']).count() == 0:
-                tweet = Analysis()
-                tweet.tweet_id = data_json['id']
-                tweet.text = data_json['text']
-                tweet.clean_text = FetchText.generate(data_json['text'])
-                tweet.user_name = data_json['user']['name']
-                tweet.user_id = data_json['user']['id']
-                tweet.user_screen_name = data_json['user']['screen_name']
-                tweet.user_location = data_json['user']['location']
-                tweet.user_created_at = datetime.datetime.strptime(data_json['user']['created_at'],
-                                                                   '%a %b %d %H:%M:%S +0000 %Y')
-                tweet.user_description = data_json['user']['description']
-                tweet.user_followers_count = data_json['user']['followers_count']
-                tweet.user_friends_count = data_json['user']['friends_count']
-                tweet.user_statuses_count = data_json['user']['statuses_count']
-                tweet.user_favourites_count = data_json['user']['favourites_count']
-                tweet.create_date_timestamp_ms = data_json['timestamp_ms']
-                tweet.create_date = datetime.datetime.strptime(data_json['created_at'],
+            # if Analysis.objects(tweet_id=data_json['id']).count() == 0:
+            tweet = Analysis()
+            tweet.tweet_id = data_json['id']
+            tweet.text = data_json['text']
+            tweet.clean_text = FetchText.generate(data_json['text'])
+            tweet.user_name = data_json['user']['name']
+            tweet.user_id = data_json['user']['id']
+            tweet.user_screen_name = data_json['user']['screen_name']
+            tweet.user_location = data_json['user']['location']
+            tweet.user_created_at = datetime.datetime.strptime(data_json['user']['created_at'],
                                                                '%a %b %d %H:%M:%S +0000 %Y')
-                tweet.source = re.findall(r'<a .*>(.*)</a>', data_json['source'])[0]
-                tweet.is_quote_status = data_json['is_quote_status']
-                tweet.media_type = data_json['entities']['media'] if data_json['entities'].get('media') else ''
-                tweet.retweet_count = data_json['retweeted_status']['retweet_count'] if data_json.get(
-                    'retweeted_status') else 0
-                tweet.favorite_count = data_json['retweeted_status']['favorite_count'] if data_json.get(
-                    'retweeted_status') else 0
-                tweet.user_mentions = data_json['entities']['user_mentions'] if data_json['entities'].get(
-                    'user_mentions') else []
-                tweet.hashtags = data_json['entities']['hashtags'] if data_json['entities'].get(
-                    'hashtags') else []
-                tweet.save()
-                # print('data saved')
-            else:
-                print('dump Tweet')
+            tweet.user_description = data_json['user']['description']
+            tweet.user_followers_count = data_json['user']['followers_count']
+            tweet.user_friends_count = data_json['user']['friends_count']
+            tweet.user_statuses_count = data_json['user']['statuses_count']
+            tweet.user_favourites_count = data_json['user']['favourites_count']
+            tweet.create_date_timestamp_ms = data_json['timestamp_ms']
+            tweet.create_date = datetime.datetime.strptime(data_json['created_at'],
+                                                           '%a %b %d %H:%M:%S +0000 %Y')
+            tweet.source = re.findall(r'<a .*>(.*)</a>', data_json['source'])[0]
+            tweet.is_quote_status = data_json['is_quote_status']
+            tweet.media_type = data_json['entities']['media'] if data_json['entities'].get('media') else ''
+            tweet.retweet_count = data_json['retweeted_status']['retweet_count'] if data_json.get(
+                'retweeted_status') else 0
+            tweet.favorite_count = data_json['retweeted_status']['favorite_count'] if data_json.get(
+                'retweeted_status') else 0
+            tweet.user_mentions = data_json['entities']['user_mentions'] if data_json['entities'].get(
+                'user_mentions') else []
+            tweet.hashtags = data_json['entities']['hashtags'] if data_json['entities'].get(
+                'hashtags') else []
+            tweet.save()
+            print('data saved')
+
         else:
             pass
 
