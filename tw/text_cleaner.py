95ba45d1bee571693b8ed9f6e33c8a84a2a3eda9
Imanoel Ali <mehot1@gmail.com>
2017-07-22 01:51:28 +0430

    check something

--- tw/text_cleaner.py
+++ tw/text_cleaner.py
@@ -16,16 +16,16 @@ class FetchText(object):
     @classmethod
     def generate(cls, text):
         # first we split sentence of tweet
-        sentences = sent_tokenize(text)
-        # now find every section of sentence
-        text_data = []
-        for sentence in sentences:
-            normalizer = Normalizer()
-            clean_sentence = cls.clean_text(text=sentence)
-            text_normal = normalizer.normalize(clean_sentence)
-            tagger = POSTagger(model=path.dirname(__file__) + '/resources/postagger.model')
-            taged = tagger.tag(word_tokenize(text_normal))
-            text_data.append(taged)
+        # sentences = sent_tokenize(text)
+        # # now find every section of sentence
+        # text_data = []
+        # for sentence in sentences:
+        #     normalizer = Normalizer()
+        #     clean_sentence = cls.clean_text(text=sentence)
+        #     text_normal = normalizer.normalize(clean_sentence)
+        #     tagger = POSTagger(model=path.dirname(__file__) + '/resources/postagger.model')
+        #     taged = tagger.tag(word_tokenize(text_normal))
+        #     text_data.append(taged)
             # for item, key in taged:
             #     if key in ['Ne', 'N', 'AJ', 'AJe']:
             #         text_data.append(item)
